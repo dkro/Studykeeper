ec62dfc11f4a6efd1c5d3beb16a492d43d609cea
Amadeus Schell <amadeus.schell17@gmail.com>
2014-12-14 02:17:28 +0100

    Revert "Revert "Revert "Improved study table"""
    This reverts commit a2c48a5ed5b5d3af624719f565172bc38c7f3222.

--- frontend/public/js/application.js
+++ frontend/public/js/application.js
@@ -13,37 +13,3 @@ var validationInput = Ember.TextField.extend({
 });
 
 Ember.Handlebars.helper('validation-input', validationInput);
-
-// Important: Ember.js has new deprecations added for "global lookup of views" in version 1.8.
-// That means: Views for example have to be named : "TestView" or "SuperTestView"
-// and are used in the HTML file by using "{{view "test"}}" or {{view "super-test"}}
-StudyManager.DatePickerView = Ember.TextField.extend({
-    _picker: null,
-
-    modelChangedValue: function(){
-        var picker = this.get("_picker");
-        if (picker){
-            picker.setDate(this.get("value"));
-        }
-    }.observes("value"),
-
-    didInsertElement: function(){
-        currentYear = (new Date()).getFullYear();
-        formElement = this.$()[0];
-        picker = new Pikaday({
-            field: formElement,
-            // Include moment.js before ember-pikaday so that formatting is working!
-            format: "DD.MM.YYYY",
-            yearRange: [1900,currentYear+2]
-        });
-        this.set("_picker", picker);
-    },
-
-    willDestroyElement: function(){
-        picker = this.get("_picker");
-        if (picker) {
-            picker.destroy();
-        }
-        this.set("_picker", null);
-    }
-});
