f6550bc3385c9cb3c4c06f9fce3b670ef3b18704
Amadeus Schell <amadeus.schell17@gmail.com>
2015-01-22 00:07:15 +0100

    Removed Home nav and commented FIXTURES out

--- frontend/public/js/controllers/login_controller.js
+++ frontend/public/js/controllers/login_controller.js
@@ -4,7 +4,7 @@ StudyManager.LoginController = Ember.Controller.extend({
     actions: {
         login: function() {
             // DEFAULT
-/*
+
             var userData = this.getProperties('username', 'password');
             var that = this;
 
@@ -20,7 +20,7 @@ StudyManager.LoginController = Ember.Controller.extend({
             }, function(error) {
                     that.set('errorMessage', 'Login failed!');
             });
-*/
+/*
 
             // MOCK
             this.set('errorMessage', null);
@@ -46,7 +46,7 @@ StudyManager.LoginController = Ember.Controller.extend({
                 this.set('currentUserId', 1);
 
                 this.transitionToRoute('dashboard');
-            }
+            }*/
         },
 
         toSignUp: function() {
